9c0ff46bbef1d7db4a8208a7dd2fd0b31df68c10
Kamal Thapa <handsomelotus1@cmail.com>
2023-12-09 13:16:31 -0500

    new update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const { errorHandler } = require("./middlewares/error-handler");
 const { limiter } = require("./middlewares/rateLimit");
 const { errors } = require("celebrate");
+const { TextEncoder, TextDecoder } = require("text-encoding");
 
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
@@ -16,6 +17,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/newsex_db", (r) => {
 });
 
 const routes = require("./routes");
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 app.use(express.json());
 app.use(cors());
