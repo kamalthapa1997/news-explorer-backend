a60fe83e8708482da919453a04a27125e248b9f2
Kamal Thapa <handsomelotus1@cmail.com>
2023-12-09 13:42:46 -0500

    new uppdate for my project

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
 const app = express();
+const util = require("util");
 const { errorHandler } = require("./middlewares/error-handler");
 const { limiter } = require("./middlewares/rateLimit");
 const { errors } = require("celebrate");
-const { TextEncoder, TextDecoder } = require("text-encoding");
+global.TextEncoder = util.TextEncoder || require("util").TextEncoder;
+global.TextDecoder = util.TextDecoder || require("util").TextDecoder;
 
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
@@ -17,8 +19,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/newsex_db", (r) => {
 });
 
 const routes = require("./routes");
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
 
 app.use(express.json());
 app.use(cors());
